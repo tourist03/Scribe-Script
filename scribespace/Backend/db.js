a9669d8733aece2a9b90bd9250657e223d56cd74
Vineet Singh <singhvineet2001@gmail.com>
2024-11-27 16:26:06 +0530

    Backend_Initiation_V1.0.1

---
+++ scribespace/Backend/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const mongoURI = "mongodb://localhost:27017/";
+
+const connectToMongo = async() => {
+    try {
+        await mongoose.connect(mongoURI);
+        console.log("Connected to mongo successfully");
+    }
+    catch (err){
+        console.error("Error connecting to mongo:", err);
+    }
+}
+
+module.exports = connectToMongo;
