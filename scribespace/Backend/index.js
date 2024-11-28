7b46240d2a48b5aba33a4587decca633ee39e5e4
Vineet Singh <singhvineet2001@gmail.com>
2024-11-28 12:29:41 +0530

    ScribeSpace Backend auth.js model started

--- scribespace/Backend/index.js
+++ scribespace/Backend/index.js
@@ -5,6 +5,11 @@ connectToMongo();
 const app = express();
 const port = 3000;
 
+//if we want to use the req.body then have to use the middleware -> express.json(), and to use middleware we use the "app.use" , now can deal with the json, can make request in json by sending content-type application/json
+
+app.use(express.json());
+
+
 //Available Routes
 
 app.use("/api/auth", require("./routes/auth"));
--- scribespace/Backend/routes/auth.js
+++ scribespace/Backend/routes/auth.js
@@ -1,12 +1,14 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const User = require("../models/User");
 
-router.get('/' , (req , res) =>{
-    obj = {
-        a : 'this',
-        number : 34
-    }
-    res.json(obj);
-})
+//Create a user using : POST "/api/auth" , Dosen't require authentication
 
-module.exports = router
+router.post("/", (req, res) => {
+  console.log(req.body);
+  const user = User(req.body);
+  user.save()
+  res.send(req.body);
+});
+
+module.exports = router;
