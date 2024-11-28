699e19be17aa55caaf9af73a7f14480c1a9a922a
Vineet Singh <singhvineet2001@gmail.com>
2024-11-28 13:57:30 +0530

    JWT Authentication auth model

--- scribespace/Backend/routes/auth.js
+++ scribespace/Backend/routes/auth.js
@@ -2,6 +2,10 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const JWT_SECRET = "$cr!be$p@ceJ%T$*cr&t";
 
 //Create a user using : POST "/api/auth/createuser" , No login required
 
@@ -29,14 +33,23 @@ router.post(
           .json({ error: "User with this email already exist" });
       }
 
+      const salt = await bcrypt.genSalt(10);
+      const secPass = await bcrypt.hash(req.body.password, salt);
+
       //create new user after all the above checks
       user = await User.create({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
+        password: secPass,
       });
 
-      res.json(user);
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const authToken = jwt.sign(data, JWT_SECRET);
+      res.json({ authToken });
     } catch (error) {
       console.error(error.message);
       res.status(500).send("Some Error Occured");
