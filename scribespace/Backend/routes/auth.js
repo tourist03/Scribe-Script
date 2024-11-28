8b90f0a351d2df23779c50154e69b2cae17b9269
Vineet Singh <singhvineet2001@gmail.com>
2024-11-28 14:13:31 +0530

    Created Login Endpoint and sending auth token

--- scribespace/Backend/routes/auth.js
+++ scribespace/Backend/routes/auth.js
@@ -52,7 +52,52 @@ router.post(
       res.json({ authToken });
     } catch (error) {
       console.error(error.message);
-      res.status(500).send("Some Error Occured");
+      res.status(500).send("Internal server Error Occured");
+    }
+  }
+);
+
+//Authenticate a user using : POST "/api/auth/login" , No login required
+
+router.post(
+  "/login",
+  [
+    body("email", "Enter a valid email address").isEmail(),
+    body("password", "Password cannot be blank").exists(),
+  ],
+  async (req, res) => {
+    // if there are errors then return bad request
+    const error = validationResult(req);
+    if (!error.isEmpty()) {
+      return res.status(400).json({ error: error.array() });
+    }
+
+    const {email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ error: "Login with correct credentials" });
+      }
+
+      const passwordcompare = await bcrypt.compare(password, user.password);
+      if (!passwordcompare) {
+        return res
+          .status(400)
+          .json({ error: "Login with correct credentials" });
+      }
+
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const authToken = jwt.sign(data, JWT_SECRET);
+      res.json({ authToken });
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internal server Error Occured");
     }
   }
 );
