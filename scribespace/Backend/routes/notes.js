dd8256aa8952ea851b58ddef9a1de797e74a2f11
Vineet Singh <singhvineet2001@gmail.com>
2024-11-29 13:18:55 +0530

    Update an existing Note_notes model

--- scribespace/Backend/routes/notes.js
+++ scribespace/Backend/routes/notes.js
@@ -50,4 +50,41 @@ router.post(
   }
 );
 
+//Route 3 : Update Notes using : PUT "/api/notes/updatenote/:id" . Login Required
+
+router.put("/updatenote/:id", fetchUser, async (req, res) => {
+  const { title, description, tag } = req.body;
+  try {
+    //create a newNote Object
+    const newNote = {};
+    if (title) {
+      newNote.title = title;
+    }
+    if (description) {
+      newNote.description = description;
+    }
+    if (tag) {
+      newNote.tag = tag;
+    }
+
+    //Find the note to be updated and update it
+    let note = await Note.findById(req.params.id);
+    if (!note) {
+      return res.status(404).send("Not Found");
+    }
+
+    if (note.user.toString() !== req.user.id) {
+      return res.status(401).send("Not Allowed");
+    }
+
+    note = await Note.findByIdAndUpdate(
+      req.params.id,
+      { $set: newNote },
+      { new: true }
+    );
+
+    res.json({ note });
+  } catch (error) {}
+});
+
 module.exports = router;
