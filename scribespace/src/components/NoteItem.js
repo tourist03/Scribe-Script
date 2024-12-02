7f161b1cf8c7aca8eea5f36cec252c01be64e1a6
Vineet Singh <singhvineet2001@gmail.com>
2024-12-02 16:23:18 +0530

    Added the font Awesome Font to the ScribeSpace NoteItems

--- scribespace/src/components/NoteItem.js
+++ scribespace/src/components/NoteItem.js
@@ -1,13 +1,24 @@
 import React from "react";
 
 const NoteItem = (props) => {
+
   const { note } = props;
   return (
     <div className="col-md-3">
       <div className="card my-3">
-        <div className="card-body">
-            <h5 className="card-title">{note.title}</h5>
-            <p className="card-text">{note.description}</p>
+        <div className="card-body position-relative">
+          <div className="position-absolute top-0 end-0 mt-2 me-2">
+            <i
+              className="fa-duotone fa-regular fa-trash-can my-2 mx-3"
+              style={{ color: "#594fa1" }}
+            ></i>
+            <i
+              className="fa-duotone fa-solid fa-user-pen mx-1"
+              style={{ color: "#5021c0" }}
+            ></i>
+          </div>
+          <h5 className="card-title">{note.title}</h5>
+          <p className="card-text">{note.description}</p>
         </div>
       </div>
     </div>
