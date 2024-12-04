ca539b29b2103258e8dbe93f9d9d9e9027210b80
Vineet Singh <singhvineet2001@gmail.com>
2024-12-04 12:44:52 +0530

    EditNoteModal Added and Refactored

--- scribespace/src/components/NoteItem.js
+++ scribespace/src/components/NoteItem.js
@@ -6,7 +6,7 @@ const NoteItem = (props) => {
   const context = useContext(noteContext);
   const { deleteNote } = context;
 
-  const { note } = props;
+  const { note, updateNote } = props;
   return (
     <div className="col-md-3">
       <div className="card my-3">
@@ -14,11 +14,17 @@ const NoteItem = (props) => {
           <div className="position-absolute top-0 end-0 mt-2 me-2">
             <i
               className="fa-duotone fa-regular fa-trash-can my-2 mx-3"
-              style={{ color: "#594fa1" }} onClick={() => {deleteNote(note._id)}}
+              style={{ color: "#594fa1" }}
+              onClick={() => {
+                deleteNote(note._id);
+              }}
             ></i>
             <i
               className="fa-duotone fa-solid fa-user-pen mx-1"
               style={{ color: "#5021c0" }}
+              onClick={() => {
+                updateNote(note);
+              }}
             ></i>
           </div>
           <h5 className="card-title">{note.title}</h5>
--- scribespace/src/components/Notes.js
+++ scribespace/src/components/Notes.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useRef } from "react";
 import noteContext from "../context/notes/noteContext";
 import NoteItem from "./NoteItem";
 import AddNote from "./AddNote";
@@ -6,16 +6,65 @@ import AddNote from "./AddNote";
 const Notes = () => {
   const context = useContext(noteContext);
   const { notes, getNotes } = context;
+
+  const updateNote = (note) => {
+    ref.current.click();
+  }
   useEffect(() => {
     getNotes();
   }, []);
+
+  const ref = useRef(null);
   return (
     <>
       <AddNote />
+      <button 
+        type="button" 
+        className="btn btn-primary d-none" 
+        data-bs-toggle="modal" 
+        data-bs-target="#exampleModal"
+        ref={ref}
+      >
+        Launch demo modal
+      </button>
+
+      <div 
+        className="modal fade" 
+        id="exampleModal" 
+        tabIndex="-1" 
+        aria-labelledby="exampleModalLabel" 
+        aria-hidden="true"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLabel">Edit Note</h5>
+              <button 
+                type="button" 
+                className="btn-close" 
+                data-bs-dismiss="modal" 
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">
+            </div>
+            <div className="modal-footer">
+              <button 
+                type="button" 
+                className="btn btn-secondary" 
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+              <button type="button" className="btn btn-primary">Save changes</button>
+            </div>
+          </div>
+        </div>
+      </div>
       <div className="row my-3">
         <h2>All Notes</h2>
         {notes.map((note) => {
-          return <NoteItem key={note._id} note={note} />;
+          return <NoteItem key={note._id} updateNote = {updateNote} note={note} />;
         })}
       </div>
     </>
