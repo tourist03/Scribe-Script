4d55b8606e65034d1c8840f2409beaf9c780afbb
Vineet Singh <singhvineet2001@gmail.com>
2024-12-05 15:18:41 +0530

    v1.1

--- scribespace/src/components/signup.js
+++ scribespace/src/components/signup.js
@@ -50,7 +50,8 @@ const Signup = (props) => {
     setCredentials({ ...credentials, [e.target.name]: e.target.value });
   };
   return (
-    <div className="container">
+    <div className="container mt-2">
+      <h2>Create your account to use ScribeSpace</h2>
       <form onSubmit={handleSubmit}>
         <div className="mb-3">
           <label htmlFor="name" className="form-label">
